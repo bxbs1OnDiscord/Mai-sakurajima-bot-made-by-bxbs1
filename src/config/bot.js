7a106fedacaa62be5876504fa48e03dab52fd61a
bxbs1OnDiscord <bxbsihallo@gmail.com>
2026-06-11 14:58:52 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made by bxb / Carrying Avbox Hub ",
+        name: "Made by bxb / Support for Avbox 24/7 ",
         // Activity type number (0 = Playing).
         type: 0, 
       },
